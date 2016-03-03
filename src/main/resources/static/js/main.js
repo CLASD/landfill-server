ca2730a21d2760e44fb51656d4e1028be3da84fd
Jeanne Pascua <jpascua93@gmail.com>
2016-03-02 16:31:37 -0800

    updated a couple stuff

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,14 @@
-/**
- * 
- */
-
-angular.module('landfill', [])
-  .controller('home', function($scope) {
-    $scope.greeting = {id: 'xxx', content: 'Hello World!'}
-})
+angular.module('landfill', ['ngRoute'])
+	.config(['$routeProvider',
+		function($routeProvider) { 
+			$routeProvider
+				.when('/', {
+					templateUrl: 'index.html'
+				})
+				.otherwise({
+					redirectTo: '/'
+				});   	 
+	}])
+	.controller('indexController', function($scope) {
+		$scope.greeting = {id: 'xxx', content: 'Hello World!'};
+	});
